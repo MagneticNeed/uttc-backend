8437f59a11f2c92c7b3a610f6b1cdd87e167365d
Magnet Need <shn154fowest@gmail.com>
2024-06-23 06:42:11 +0900

    modification

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,18 +15,16 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	// .envファイルの読み込み, デプロイ時はコメントアウト
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+	// // .envファイルの読み込み, デプロイ時はコメントアウト
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file: %v", err)
 
-	}
+	// }
 	// DB接続のための準備
 	mysqlUser := os.Getenv("MYSQL_USER")
 	mysqlPwd := os.Getenv("MYSQL_PASSWORD")
